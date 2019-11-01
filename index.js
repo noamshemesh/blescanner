9f6f8ca257f05e34c8cc8be2aa50e5c3af994e42
Noam Shemesh <noamshemesh@users.noreply.github.com>
2019-11-01 13:07:25 +0200

    Clear the previous interval

--- index.js
+++ index.js
@@ -3,25 +3,39 @@ var mqtt = require('mqtt')
 var client  = mqtt.connect('mqtt://localhost:1883')
 var lastTime = new Date()
 
+console.log('Attempt to connect to mqtt')
+var oldReporter;
 client.on('connect', function () {
+  console.log('Connected to mqtt')
   client.subscribe('presence', function (err) {
-    if (!err) {
+    console.log('Start scanning')
+    if (err) {
+      console.log('ERR: ', err)
+    } else {
       Bleacon.startScanning();
       Bleacon.on('discover', function(event) {
-        if (event.uuid === 'beaconuuid') {
+        if (event.uuid === 'b9407f30f5f8466eaff925556b57fe6d') {
           lastTime = new Date()
         }
       });
     }
   })
+
+  if (!oldReporter) {
+    clearInterval(oldReporter);
+  }
+
+  oldReporter = setInterval(function () {
+    if ((new Date() - lastTime) <= 30000) {
+      client.publish('location/brian', 'home')
+      console.log('Brian is home')
+    } else {
+      client.publish('location/brian', 'not_home')
+      console.log('Brian is away')
+    }
+  }, 10000)
 })
 
-setInterval(function () {  
-  if ((new Date() - lastTime) <= 30000) {
-    client.publish('location/brian', 'home')
-    console.log('Brian is home')
-  } else {
-    client.publish('location/brian', 'not_home')
-    console.log('Brian is away')
-  }
-}, 10000)
+client.on('error', function (err) {
+  console.log('ERR when connecting', err)
+})
